f6ae98255a127500e954981bc097c323eec3dd0b
Jongarde <jon.gardeazabal@opendeusto.es>
2021-11-07 10:28:25 +0100

    errores models

--- mysite/SkiStickApp/models.py
+++ mysite/SkiStickApp/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Localizacion(models.Model):
-    id = models.CharField(max_length=12)
+    id = models.CharField(max_length=12, primary_key=True)
     nombre = models.CharField(max_length=30)
 
 class Estacion(models.Model):
-    id = models.CharField(max_length=12)
+    id = models.CharField(max_length=12, primary_key=True)
     nombre = models.CharField(max_length=30)
     puntos_alquiler = models.IntegerField(default=0)
     localizacion = models.ForeignKey(Localizacion, on_delete=models.CASCADE)
 
-class Pista(models.model):
-    id = models.CharField(max_length=12)
-    color_tipo = models.CharField(max_lenght=30)
+class Pista(models.Model):
+    id = models.CharField(max_length=12, primary_key=True)
+    color_tipo = models.CharField(max_length=30)
     estacion = models.ForeignKey(Estacion, on_delete=models.CASCADE)
