10e1d344d8c9326cbbf39ecacd46f2a408c7775a
Jongarde <jon.gardeazabal@opendeusto.es>
2021-11-07 10:33:45 +0100

    admin.py

--- mysite/SkiStickApp/admin.py
+++ mysite/SkiStickApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from.models import Localizacion, Estacion, Pista
 
 # Register your models here.
+admin.site.register(Estacion)
+admin.site.register(Localizacion)
+admin.site.register(Pista)
