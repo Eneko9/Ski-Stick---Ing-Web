73e2914f7be6010f14a4d32d8087471c6d4dec5e
Jongarde <jon.gardeazabal@opendeusto.es>
2021-11-10 20:03:45 +0100

    detalles_estacion más completo y la lista de pistas no tiene elementos repetidos

--- mysite/SkiStickApp/views.py
+++ mysite/SkiStickApp/views.py
@@ -26,9 +26,31 @@ def lista_estaciones(request):
 def detalle_estacion(request, id_estacion):
     det_estacion = get_object_or_404(Estacion, pk = id_estacion)
     pistas = get_list_or_404(Pista, estacion = id_estacion)
+
+    lista_tipos = []
+    for p in pistas:
+        lista_tipos.append(p.color_tipo)
+
+    colors = get_list_or_404(Pista)
+    lista_colors = []
+    for c in colors:
+        if c.color_tipo not in lista_colors:
+            lista_colors.append(c.color_tipo)
+    
+    lista_strings = []
+
+    for c in lista_colors:
+        if lista_tipos.count(c) != 0:
+            elemento_pista = c + " (" + str(lista_tipos.count(c)) + ")"
+            lista_strings.append(elemento_pista)
+        #lista_conts.append(lista_tipos.count(c))
+
+
+
     context ={
         'det_estacion' : det_estacion,
-        'pistas' : pistas
+        'pistas' : pistas,
+        'lista_strings' : lista_strings
     }
     return render(request, "detalleEstacion.html", context)
 
@@ -48,10 +70,15 @@ def detalle_localizacion(request, id_localizacion):
     }
     return render(request, "detalleLocalizacion.html", context)
 
-def lista_Pistas(request):
+def lista_pistas(request):
     colors = get_list_or_404(Pista)
+    colors2 = []
+    for c in colors:
+        if c.color_tipo not in colors2:
+            colors2.append(c.color_tipo)
+    
     context={
-        'colors': colors
+        'colors2': colors2
     }
     return render(request, "listaPistas.html", context)
 
