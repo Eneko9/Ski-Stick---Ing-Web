81de7e873083db25c4574e20b771590f084677f9
Jongarde <jon.gardeazabal@opendeusto.es>
2021-12-12 18:43:37 +0100

    modo oscuro

--- mysite/SkiStickApp/static/js/darkmode.js
+++ mysite/SkiStickApp/static/js/darkmode.js
@@ -3,8 +3,30 @@ function cambiar_modo(){
   let header = document.getElementById("header");
   header.style.backgroundColor = "black";
 
+  document.body.style.backgroundColor = "#1D1D1D";
 
-  //document.getElementById("header").style.backgroundColor = "black";
+  let sitec = document.getElementById("site_content");
+  sitec.style.backgroundColor = "#777";
+  sitec.style.color = "white";
+
+  let menu = document.getElementById("menubar");
+  menu.style.backgroundColor = "midnightblue";
+
+  let titprincipal = document.getElementById("logo_colour");
+  titprincipal.style.color = "midnightblue";
+
+  let titulos = document.getElementsByTagName('h2');
+  for(var i = 1; i<titulos.length; i++){
+    titulos[i].style.color = "midnightblue";
+  }
+
+  let footer = document.getElementById("footer");
+  footer.style.backgroundColor = "midnightblue"; 
+
+  let cuadro = document.getElementById("modo");
+  cuadro.style.border = "1px solid white";
+  
+  //$("#modoN").attr("src", "../modoclaro.png");
   
 }
 $(document).ready(function() {
