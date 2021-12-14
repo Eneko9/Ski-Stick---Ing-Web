4bb29247a12e44dce838f017a0a49e17272348a4
Jongarde <jon.gardeazabal@opendeusto.es>
2021-12-14 09:15:27 +0100

    modoOscuro terminado

--- mysite/SkiStickApp/static/js/darkmode.js
+++ mysite/SkiStickApp/static/js/darkmode.js
@@ -1,35 +1,47 @@
-function cambiar_modo(){
+var modo = new Boolean(true);
+
+function cambiar_modo() {
   //TODO cambiar los estilos por modo oscuro
-  let header = document.getElementById("header");
-  header.style.backgroundColor = "black";
 
-  document.body.style.backgroundColor = "#1D1D1D";
+  if (modo) {
+    let header = document.getElementById("header");
+    header.style.backgroundColor = "black";
 
-  let sitec = document.getElementById("site_content");
-  sitec.style.backgroundColor = "#777";
-  sitec.style.color = "white";
+    document.body.style.backgroundColor = "#1D1D1D";
 
-  let menu = document.getElementById("menubar");
-  menu.style.backgroundColor = "midnightblue";
+    let sitec = document.getElementById("site_content");
+    sitec.style.backgroundColor = "#777";
+    sitec.style.color = "white";
 
-  let titprincipal = document.getElementById("logo_colour");
-  titprincipal.style.color = "midnightblue";
+    let menu = document.getElementById("menubar");
+    menu.style.backgroundColor = "midnightblue";
 
-  let titulos = document.getElementsByTagName('h2');
-  for(var i = 1; i<titulos.length; i++){
-    titulos[i].style.color = "midnightblue";
+    let titprincipal = document.getElementById("logo_colour");
+    titprincipal.style.color = "midnightblue";
+
+    let titulos = document.getElementsByTagName('h2');
+    for (var i = 1; i < titulos.length; i++) {
+      titulos[i].style.color = "midnightblue";
+    }
+
+    let footer = document.getElementById("footer");
+    footer.style.backgroundColor = "midnightblue";
+
+    let cuadro = document.getElementById("modo");
+    cuadro.style.border = "1px solid white";
+
+    $("#modoN").attr("src", "/static/sol.png");
+
+    modo = false;
+  }else{
+    location.reload();
+    modo = true;
   }
-
-  let footer = document.getElementById("footer");
-  footer.style.backgroundColor = "midnightblue"; 
-
-  let cuadro = document.getElementById("modo");
-  cuadro.style.border = "1px solid white";
-  
-  //$("#modoN").attr("src", "../modoclaro.png");
-  
 }
-$(document).ready(function() {
-    var boton = document.getElementById('bModo');
-    boton.onclick = cambiar_modo;
-  });
+
+
+$(document).ready(function () {
+  var boton = document.getElementById('bModo');
+  boton.onclick = cambiar_modo;
+  
+});
